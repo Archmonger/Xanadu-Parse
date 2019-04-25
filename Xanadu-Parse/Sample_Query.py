8779e3dc7060c7e49e7b15e2734238baba583a34
Archmonger <16909269+Archmonger@users.noreply.github.com>
2019-04-24 18:50:11 -0700

    modularize

--- Xanadu-Parse/Sample_Query.py
+++ Xanadu-Parse/Sample_Query.py
@@ -2,12 +2,6 @@ import requests
 import feedparser
 import re
 
-# Sending an API request to xanadu.cf
-parameters = {"apikey": "7o0tagxqeryipw4oqbbn1je2az11qnph", "cat": "5000,5030,5040,5060,5070,2000,2020,2040,2050,2070", "q": input("Search: ")}
-url = "https://xanadu.cf/api/v2.0/indexers/all/results/torznab/api"
-response = requests.get(url, parameters)
-search_dict = {}
-
 def standardize_title(title):
     if (title.count(' ') <= 1):
             # Standardize periods to spaces
@@ -31,15 +25,25 @@ def standardize_title(title):
 
     return title;
 
-if response.status_code == 200:
-    # Parsing the contents of the request (RSS feed) into human-readable format using feedparser
-    rss = feedparser.parse(response.content)
-    for post in rss.entries:
-        title = standardize_title(post.title)
-        search_dict[title] = post.comments
+def main():
+    # Creating an API request to xanadu.cf
+    parameters = {"apikey": "7o0tagxqeryipw4oqbbn1je2az11qnph", "cat": "5000,5030,5040,5060,5070,2000,2020,2040,2050,2070", "q": input("Search: ")}
+    url = "https://xanadu.cf/api/v2.0/indexers/all/results/torznab/api"
+    response = requests.get(url, parameters)
+    search_dict = {}
 
-    # Print all titles
-    for title in search_dict:
-        print(title)
-else:
-    print("Connection Failed!")
+    if response.status_code == 200:
+        # Parsing the contents of the request (RSS feed) into human-readable format using feedparser
+        rss = feedparser.parse(response.content)
+        for post in rss.entries:
+            title = standardize_title(post.title)
+            search_dict[title] = post.comments
+
+        # Print all titles
+        for title in search_dict:
+            print(title)
+    else:
+        print("Connection Failed!")
+
+if (__name__== "__main__"):
+    main()
