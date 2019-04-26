fb9b025a0b3e06b3e354a3f2f09c098490f05261
Archmonger <16909269+Archmonger@users.noreply.github.com>
2019-04-26 13:31:17 -0700

    modularize

--- Xanadu-Parse/Sample_Query.py
+++ Xanadu-Parse/Sample_Query.py
@@ -1,49 +1,35 @@
 import requests
 import feedparser
 import re
-
-def standardize_title(title):
-    if (title.count(' ') <= 1):
-            # Standardize periods to spaces
-            if (title.count('.') > 0 and title.count('.') > title.count('_')): 
-                a = re.compile(r"\w\.\.") # abc..  ->  abc .
-                b = re.compile(r"\.\.\w") # ..abc  ->  . abc
-                c = re.compile(r"\w\.\w") # abc.abc  ->  abc abc
-                d = re.compile(r" (mkv|mp4|m4p|m4v|mpg|mp2|mpeg|mpe|mpv|svi|divx|flv|f4v|f4p|f4a|f4b|avi|wmv|mov|webm|vob|yuv)$") # " mp4" -> .mp4
-                for match in a.finditer(title):
-                    title = title[:match.start()+1] + " " + title[match.start()+2:]
-                for match in b.finditer(title):
-                    title = title[:match.start()+1] + " " + title[match.start()+2:]
-                for match in c.finditer(title):
-                    title = title[:match.start()+1] + " " + title[match.start()+2:]
-                for match in d.finditer(title):
-                    title = title[:match.start()] + "." + title[match.start()+1:]
-
-            # Standardize underscores to spaces
-            elif (title.count('_') > 0): 
-                title = title.replace("_", " ")
-
-    return title;
+from Standardize import standardize_title
 
 def main():
-    # Creating an API request to xanadu.cf
-    parameters = {"apikey": "7o0tagxqeryipw4oqbbn1je2az11qnph", "cat": "5000,5030,5040,5060,5070,2000,2020,2040,2050,2070", "q": input("Search: ")}
-    url = "https://xanadu.cf/api/v2.0/indexers/all/results/torznab/api"
-    response = requests.get(url, parameters)
-    search_dict = {}
+    continueProgram = True;
+    while continueProgram:
+        # Creating an API request to xanadu.cf
+        parameters = {"apikey": "7o0tagxqeryipw4oqbbn1je2az11qnph", "cat": "5000,5030,5040,5060,5070,2000,2020,2040,2050,2070", "q": input("Search: ")}
+        url = "https://xanadu.cf/api/v2.0/indexers/all/results/torznab/api"
+        response = requests.get(url, parameters)
+        search_dict = {}
 
-    if response.status_code == 200:
-        # Parsing the contents of the request (RSS feed) into human-readable format using feedparser
-        rss = feedparser.parse(response.content)
-        for post in rss.entries:
-            title = standardize_title(post.title)
-            search_dict[title] = post.comments
+        if response.status_code == 200:
+            # Parsing the contents of the request (RSS feed) into human-readable format using feedparser
+            rss = feedparser.parse(response.content)
+            for post in rss.entries:
+                title = standardize_title(post.title)
+            
+                if title is not None:
+                    search_dict[title] = post.comments
 
-        # Print all titles
-        for title in search_dict:
-            print(title)
-    else:
-        print("Connection Failed!")
+            # Print all titles
+            for title in search_dict:
+                print(title)
+                
+        else:
+            print("Connection Failed!")
+
+        if input('Do you want to continue (y/n): ') in ('n', 'N', 'no', 'NO', 'No'):
+            continueProgram = False
 
 if (__name__== "__main__"):
     main()
