d2ae4c4eba848e1e0d04e25eb116f1f67bc0dc07
Archmonger <>
2019-05-09 00:50:32 -0700

    It accepts it! Just need to one hot encode...

--- Xanadu-Parse/XanaduParse.py
+++ Xanadu-Parse/XanaduParse.py
@@ -1,8 +1,11 @@
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
-from keras.layers import Flatten
 from keras.optimizers import RMSprop
+#from keras.layers import Flatten
+#from keras.preprocessing.text import text_to_word_sequence
+#from keras.preprocessing.text import one_hot
+#from keras.preprocessing.text import Tokenizer
 import pandas as pd 
 import numpy as np
 
@@ -14,13 +17,6 @@ line_count = train_input['Altered File Name'].count()
 train_input = train_input.values.reshape(1,line_count,2)
 train_output = train_output.values.reshape(1,line_count,4)
 
-#automater_inputs = Automater(data_type_dict=input_vars, output_var=None)
-#automater_outputs = Automater(data_type_dict=output_vars, output_var=None)
-#automater_inputs.fit(train_input)
-#automater_outputs.fit(train_output)
-#x = automater_inputs.transform(train_input)
-#y = automater_outputs.transform(train_output)
-
 ## Testing dataset
 #test = pd.read_csv(r"C:\Users\Markg\Downloads\Repositories\Xanadu-Parse\Xanadu-Parse\test.csv", sep='|')
 #test_input = test[['Altered File Name','Content Type']]
@@ -28,8 +24,9 @@ train_output = train_output.values.reshape(1,line_count,4)
 
 # Build the model: a single LSTM
 model = Sequential()
+model.add(LSTM(4, input_shape=(line_count,2), return_sequences=True))
 model.add(LSTM(128, input_shape=(line_count,2), return_sequences=True))
-#model.add(Dense(128, activation='softmax'))
+model.add(Dense(20, activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer=RMSprop(lr=0.01))
 
 model.fit(train_input, train_output)
