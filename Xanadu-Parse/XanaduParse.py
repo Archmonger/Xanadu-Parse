fe28123efc06f3d1e5649f4bb7d1d44b4a49328d
Archmonger <>
2019-05-08 17:09:29 -0700

    :(

--- Xanadu-Parse/XanaduParse.py
+++ Xanadu-Parse/XanaduParse.py
@@ -10,6 +10,7 @@ import numpy as np
 train = pd.read_csv(r'C:\Users\Markg\Downloads\Repositories\Xanadu-Parse\Xanadu-Parse\train.csv', sep='|')
 train_input = train[['Altered File Name','Content Type']]
 train_output = train[['Title','Season','Episode','Resolution']]
+line_count = train_input['Altered File Name'].count()
 
 # Testing dataset
 test = pd.read_csv(r"C:\Users\Markg\Downloads\Repositories\Xanadu-Parse\Xanadu-Parse\test.csv", sep='|')
@@ -18,7 +19,7 @@ test_output = test[['Title','Season','Episode','Resolution']]
 
 # Build the model: a single LSTM
 model = Sequential()
-model.add(LSTM(128, input_shape=(train_input['Altered File Name'].count(), 2), return_sequences=True))
+model.add(LSTM(128, input_shape=(line_count, 2), return_sequences=True))
 model.add(Dense(128, activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer=RMSprop(lr=0.01))
 
