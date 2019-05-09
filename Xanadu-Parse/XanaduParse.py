0ed2c53f6e9f68963c079fe6729149f2fe6e3b10
Archmonger <>
2019-05-08 23:49:08 -0700

    one small step for man...

--- Xanadu-Parse/XanaduParse.py
+++ Xanadu-Parse/XanaduParse.py
@@ -6,16 +6,19 @@ from keras.optimizers import RMSprop
 import pandas as pd 
 import numpy as np
 
-# Training dataset
-train = pd.read_csv(r'C:\Users\Markg\Downloads\Repositories\Xanadu-Parse\Xanadu-Parse\train.csv', sep='|')
-train_input = train[['Altered File Name','Content Type']]
-train_output = train[['Title','Season','Episode','Resolution']]
-line_count = train_input['Altered File Name'].count()
+data_type_dict = {'text':['altered file name','content type','altered file name','content type']}
+output_vars = ['title','season','episode','resolution']
 
-# Testing dataset
-test = pd.read_csv(r"C:\Users\Markg\Downloads\Repositories\Xanadu-Parse\Xanadu-Parse\test.csv", sep='|')
-test_input = test[['Altered File Name','Content Type']]
-test_output = test[['Title','Season','Episode','Resolution']]
+# Training dataset
+train = pd.read_csv(r'c:\users\markg\downloads\repositories\xanadu-parse\xanadu-parse\train.csv', sep='|')
+train_input = train[['altered file name','content type']]
+train_output = train[['title','season','episode','resolution']]
+line_count = train_input['altered file name'].count()
+
+## Testing dataset
+#test = pd.read_csv(r"C:\Users\Markg\Downloads\Repositories\Xanadu-Parse\Xanadu-Parse\test.csv", sep='|')
+#test_input = test[['Altered File Name','Content Type']]
+#test_output = test[['Title','Season','Episode','Resolution']]
 
 # Build the model: a single LSTM
 model = Sequential()
